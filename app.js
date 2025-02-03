1096ba28929753b2d15986254dd012b34ddde19b
Guille Rivas <a.guille.rivas@gmail.com>
2025-02-03 12:14:30 -0300

    funcion agregar amigos terminada

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array para almacenar los nombres
+let amigos = [];
+
+//Funcion para capturar el valor del campo de entrada
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById("amigo").value;
+
+    if (amigoIngresado == "") {
+        alert("Por favor, inserte un nombre.");
+    }else{
+        amigos.push(amigoIngresado);
+        document.getElementById("amigo").value = " ";
+        console.log(amigos);
+    }
+}
+
