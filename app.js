5d127b8808b834fd3d715f57440e15e04a8905a6
Guille Rivas <a.guille.rivas@gmail.com>
2025-02-04 09:08:38 -0300

    Termine funcion para actualizar lista

--- app.js
+++ app.js
@@ -7,12 +7,34 @@ let amigos = [];
 function agregarAmigo(){
     let amigoIngresado = document.getElementById("amigo").value;
 
+    //Valido que el campo no este vacio
     if (amigoIngresado == "") {
         alert("Por favor, inserte un nombre.");
+
+    // Agrego el elemento al Array y vacio el campo
     }else{
         amigos.push(amigoIngresado);
         document.getElementById("amigo").value = " ";
         console.log(amigos);
+
+        // Agregamos la funcion de agregar en el onclick
+        actualizarLista();
     }
 }
 
+// Funcion para agregar amigos a la lista
+function actualizarLista() {
+    // Obtengo la el elemento de la lista y lo vacio
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = " ";
+
+    // Recorro el Array con los nombres y los sumo a la lista.
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li")
+
+        li.textContent = amigos[i];
+
+        lista.appendChild(li);
+        
+    }
+}
